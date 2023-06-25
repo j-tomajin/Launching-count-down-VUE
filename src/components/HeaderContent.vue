724df4e348991c63fc9090907cb9a75c3c191e51
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-25 23:16:41 +0800

    renamed

---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'HeaderContent',
+        props: {
+            text: {
+                type: String,
+                default: 'This is header',
+            },
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <h1 class="header">{{ text }}</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    @use '../assets/scss/utilities/' as *;
+
+    .header {
+        text-align: center;
+    }
+</style>
